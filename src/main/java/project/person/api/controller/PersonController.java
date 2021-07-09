b7ef17a7d4c3b0ff97ff9ff71201d6e04359102a
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-07-09 12:08:16 -0300

    Project Created

---
+++ src/main/java/project/person/api/controller/PersonController.java
@@ -0,0 +1,13 @@
+package project.person.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    public String getPerson(){
+        return "API test";
+    }
+}
---
+++ src/test/java/project/person/api/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.person.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
